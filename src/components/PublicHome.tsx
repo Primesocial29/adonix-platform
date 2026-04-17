53563462986db18eefcee5caf8a8534cda83ec7b
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-16 23:54:16 -0400

    Updated PublicHome.tsx

--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -424,17 +424,14 @@ We are actively working to improve accessibility. If you experience any issues, 
         {/* Left Side - Solid Black Background */}
         <div className="relative w-3/5 bg-black flex flex-col justify-center">
           <div className="relative z-10 max-w-2xl ml-auto mr-0 px-8">
-           <<img 
-  {/* Logo - CONTROL SIZE & POSITION WITH PIXELS */}
+            {/* Logo - CONTROL SIZE & POSITION WITH PIXELS */}
 <div style={{ 
-  marginBottom: '24px',
-  marginLeft: '0px'
+  marginBottom: '24px',      // ← Change to move DOWN (increase) or UP (decrease)
+  marginLeft: '0px'          // ← Change to move RIGHT (positive) or LEFT (negative)
 }}>
   <img 
-    src="/adonixlogo.png" 
-    alt="Adonix Logo" 
     style={{
-      width: '300px',        // ← CHANGE THIS NUMBER (200px, 300px, 400px, etc.)
+      width: '340px',        // ← Change to make BIGGER (increase) or SMALLER (decrease)
       height: 'auto',
       background: 'transparent'
     }}
