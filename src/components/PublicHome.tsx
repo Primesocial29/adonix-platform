c22b668c6ac475c0237c4669245f23113b911e0e
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-17 16:17:49 -0400

    Updated PublicHome.tsx

--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -505,7 +505,7 @@ We are actively working to improve accessibility. If you experience any issues, 
             width: '780px',
             height: '780px',
             position: 'relative',
-            top: '80px'
+            top: '40x'
           }}>
             <img 
               src="/girl_image_backgroundinterface.jpg" 
