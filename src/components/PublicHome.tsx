a11f5b9025845544eb075ec2625b18af9d918136
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-17 15:21:04 -0400

    Updated PublicHome.tsx

--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -430,7 +430,7 @@ We are actively working to improve accessibility. If you experience any issues, 
 {/* ========================================================= */}
 <div style={{ 
   marginBottom: '-105px',    // ← Change to move DOWN (increase) or UP (decrease)
-  marginLeft: '-17px'        // ← Change to move RIGHT (positive) or LEFT (negative)
+  marginLeft: '-2px'        // ← Change to move RIGHT (positive) or LEFT (negative)
 }}>
   <img 
     src="/adonixlogo.png" 
