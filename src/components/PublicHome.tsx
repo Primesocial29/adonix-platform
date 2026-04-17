54547f0d5c4853bb9385bdb5a4576ffbce4fea12
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-17 15:38:50 -0400

    Updated PublicHome.tsx

--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -449,7 +449,7 @@ We are actively working to improve accessibility. If you experience any issues, 
                 src="/adonixlogo.png" 
                 alt="Adonix Logo" 
                 style={{
-                  width: '900px',
+                  width: '250px',
                   height: 'auto',
                   maxWidth: 'none',
                   background: 'transparent',
