05452dd1c7559259b65090fc8a367dc10c80745e
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-17 16:12:33 -0400

    Updated PublicHome.tsx

--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -471,7 +471,7 @@ We are actively working to improve accessibility. If you experience any issues, 
 }}>
   <div className="text-left">
     <p className="text-1.75xl md:text-5xl font-bold text-white leading-tight">
-      VerifiedPublic
+      Verified Public
     </p>
     <p className="text-2xl md:text-5xl font-bold text-red-500 leading-tight mt-1">
       Meetups<span className="text-white">|</span>
