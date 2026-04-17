d7282814131058709d7af8c9fab089f74a4bd854
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-17 17:58:06 -0400

    Updated PublicHome.tsx

--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -434,7 +434,7 @@ We are actively working to improve accessibility. If you experience any issues, 
             {/* PNG LOGO - MOVED HIGHER (more negative margin = higher) */}
             {/* ========================================================= */}
             <div style={{ 
-              marginBottom: '160px',    // Changed from -160px to -220px (moved HIGHER)
+              marginBottom: '120px',    // Changed from -160px to -220px (moved HIGHER)
               marginLeft: '-17px'
             }}>
               <img 
