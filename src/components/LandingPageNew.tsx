7a4226aaa0c9ed95da51e3bf59a991436a27acdf
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-04-21 00:56:06 -0400

    Updated LandingPageNew.tsx

--- src/components/LandingPageNew.tsx
+++ src/components/LandingPageNew.tsx
@@ -124,7 +124,7 @@ Zero-Tolerance Policy: Private location requests, harassment, or unsafe behavior
           <img 
             src="/GIRL2.png" 
             alt="Social Fitness Member" 
-            className="w-[95%] h-auto object-contain brightness-110 contrast-125"
+            className="w-[70%] h-auto object-contain brightness-110 contrast-125"
           
           />
         </div>
