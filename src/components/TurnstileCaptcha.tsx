f24ce9058701fa7f62619735de635f6f9259f29b
Primesocial29 <257198489+Primesocial29@users.noreply.github.com>
2026-05-04 18:59:14 -0400

    Updated TurnstileCaptcha.tsx

--- src/components/TurnstileCaptcha.tsx
+++ src/components/TurnstileCaptcha.tsx
@@ -1,5 +1,4 @@
 import { Turnstile } from '@marsidev/react-turnstile';
-import { useState, useEffect } from 'react';
 
 interface TurnstileCaptchaProps {
   onSuccess: (token: string) => void;
@@ -8,38 +7,9 @@ interface TurnstileCaptchaProps {
 }
 
 export default function TurnstileCaptcha({ onSuccess, onError, onExpire }: TurnstileCaptchaProps) {
-  const [siteKey, setSiteKey] = useState<string>('');
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const isDev = import.meta.env.DEV;
-    const devKey = import.meta.env.VITE_TURNSTILE_SITE_KEY_DEV;
-    const prodKey = import.meta.env.VITE_TURNSTILE_SITE_KEY_PROD;
-    
-    const fallbackDevKey = '0x4AAAAAAAC85hzmi4sizIJ-y';
-    const fallbackProdKey = '0x4AAAAAAAS5hzmj4sizJ-y';
-    
-    let key = '';
-    if (isDev) {
-      key = devKey || fallbackDevKey;
-      console.log('Using Turnstile DEV key');
-    } else {
-      key = prodKey || fallbackProdKey;
-      console.log('Using Turnstile PROD key');
-    }
-    
-    setSiteKey(key);
-    setIsLoading(false);
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center p-4">
-        <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-red-500"></div>
-      </div>
-    );
-  }
-
+  // Use Cloudflare's test key that ALWAYS works in development
+  const siteKey = "1x00000000000000000000AA";
+  
   return (
     <Turnstile
       siteKey={siteKey}
